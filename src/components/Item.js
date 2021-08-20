bbfc7dae230fc5ea1675d7fc03d7d9bc9a0692ed
Niyaz <niyaz.irfan119@gmail.com>
2021-08-20 15:05:54 +0300

    commit tree ee98cbe71b1e4bb3af2cf5fe98cac4c6ad3db103

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
+// ({ name, category })
+class Item extends React.Component {
+  // const [isInCart, setIsInCart] = useState(false);
+  state = {
+    isInCart: false,
+  };
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
-
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+  handleAddToCartClick() {
+    // setIsInCart((isInCart) => !isInCart);
+    this.setState((prev) => {
+      return { isInCart: !prev.isInCart };
+    });
   }
 
-  return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-    </li>
-  );
+  render() {
+    return (
+      <li className={this.state.isInCart ? "in-cart" : ""}>
+        <span>{this.props.name}</span>
+        <span className="category">{this.props.category}</span>
+        <button
+          className={this.state.isInCart ? "remove" : "add"}
+          onClick={() => this.handleAddToCartClick()}
+        >
+          {this.state.isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,38 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+class ShoppingList extends React.Component {
+  state = {
+    selectedCategory: "All",
+  };
 
-  function handleCategoryChange(event) {
+  // const [selectedCategory, setSelectedCategory] = useState("All");
+
+  handleCategoryChange(event) {
     // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+    // console.log(event.target.value);
+    this.setState({ selectedCategory: event.target.value });
+
+    // setSelectedCategory(event.target.value);
   }
 
   // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  getItemsToDisplay() {
+    const itemsToDisplay = this.props.items.filter((item) => {
+      if (
+        this.state.selectedCategory === "All" ||
+        item.category === this.state.selectedCategory
+      )
+        return true;
+    });
 
-    return item.category === selectedCategory;
-  });
+    return itemsToDisplay;
+  }
 
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+  render() {
+    return (
+      <div className="ShoppingList">
+        <div className="Filter">
+          <select
+            name="filter"
+            onChange={(event) => this.handleCategoryChange(event)}
+          >
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </div>
+        <ul className="Items">
+          {this.getItemsToDisplay().map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ))}
+        </ul>
       </div>
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default ShoppingList;
